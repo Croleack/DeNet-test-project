fe146dd51e66bf449acdf32affae11e6350cc2c9
Радик Хазиев <121854191+Croleack@users.noreply.github.com>
2023-11-15 19:50:48 +0500

    Initial Commit

---
+++ DeNet/DeNetApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeNetApp.swift
+//  DeNet
+//
+//  Created by Enzhe Gaysina on 15.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct DeNetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
